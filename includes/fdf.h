70755f1aa9b74db5c8a30f1fa20bb4f6d0fa6cf6
Esther DEVEZE <edeveze@student.42.fr>
2017-01-24 21:54:45 +0100

    rep includes with fdf header created

---
+++ includes/fdf.h
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fdf.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: edeveze <edeveze@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/01/24 17:27:21 by edeveze           #+#    #+#             */
+/*   Updated: 2017/01/24 21:52:30 by edeveze          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FDF_H
+# define FDF_H
+
+/*
+** Libraries for open, read, write and close.
+*/
+
+# include <fcntl.h>
+# include <sys/uio.h>
+# include <unistd.h>
+
+/*
+** Library for malloc, free and exit.
+*/
+
+# include <stdlib.h>
+
+/*
+** Libraries for perror adnd strerror.
+*/
+
+# include <stdio.h>
+# include <string.h>
+
+# include <math.h>
+# include "mlx.h"
+
+#endif
