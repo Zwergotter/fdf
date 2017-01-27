38b1aaaa4672560ff7aad13845dca9f554d0d299
Esther DEVEZE <edeveze@student.42.fr>
2017-01-27 16:36:50 +0100

    modification to init array of positions and to work only with one file as argument

--- fdf.c
+++ fdf.c
@@ -16,26 +16,34 @@ void init_env(t_env *env)
 {
 	env->x = 50;
 	env->y = 50;
-	env->z = 0;
 	env->mlx = mlx_init();
 	env->win = mlx_new_window(env->mlx, WIDTH, LENGTH, "fdf");
+}
+
+void init_array(t_pos **array_pos, char **av)
+{
+		int		fd;
+
+		fd = open(av[1], O_RDONLY);
+		array_pos = read_file(fd);
+		close(fd);
 }
 
 int	main(int ac, char **av)
 {
 	t_env *env;
-	int		fd;
+	t_pos **array_pos;
 
 	if (ac == 2)
 	{
-		fd = open(av[1], O_RDONLY);
-		read_file(fd);
+		if (!(env = (t_env*)malloc(sizeof(t_env))) ||
+			!(array_pos = (t_pos**)malloc(sizeof(t_pos))))
+			return (-1);
+		init_array(array_pos, &av[1]);
+		init_env(env);
+		while (env->y++ < 150)
+			mlx_pixel_put(env->mlx, env->win, env->x, env->y, 00255000000);
+		mlx_loop(env->mlx);	
 	}
-	if (!(env = (t_env*)malloc(sizeof(t_env))))
-		return (-1);
-	init_env(env);
-	while (env->y++ < 150)
-		mlx_pixel_put(env->mlx, env->win, env->x, env->y, 00255000000);
-	mlx_loop(env->mlx);
 	return (0);
 }
