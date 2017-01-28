f5bad7ac31eb9b255c10a1aa31cf851e0a45ffdc
Esther DEVEZE <edeveze@student.42.fr>
2017-01-28 14:51:38 +0100

    calculting input height function added

--- fdf.c
+++ fdf.c
@@ -12,35 +12,58 @@
 
 #include "fdf.h"
 
-void init_env(t_env *env)
+int     map_length(int fd)
 {
+    char buf[1];
+    int i;
+
+    i = 0;
+    while (read(fd, buf, 1))
+    {
+        if (buf[0] == '\n')
+            i++;
+    }
+    if (buf[0] != '\n')
+        i++;
+	printf("map lenght is %d\n", i);
+    return (i);
+}
+
+void init_env(t_env *env, char *map)
+{
+	int fd;
+
+	fd = open(map, O_RDONLY);
 	env->x = 50;
 	env->y = 50;
 	env->mlx = mlx_init();
 	env->win = mlx_new_window(env->mlx, WIDTH, LENGTH, "fdf");
+	env->len = map_length(fd);
+	close(fd);
 }
 
-void init_array(t_pos **array_pos, char **av)
+void init_array(char ***array_pos, char **av, int length)
 {
 		int		fd;
 
 		fd = open(av[1], O_RDONLY);
-		array_pos = read_file(fd);
+		array_pos = read_file(array_pos, fd, length);
 		close(fd);
 }
 
 int	main(int ac, char **av)
 {
 	t_env *env;
-	t_pos **array_pos;
+	char ***array_pos;
 
 	if (ac == 2)
 	{
-		if (!(env = (t_env*)malloc(sizeof(t_env))) ||
-			!(array_pos = (t_pos**)malloc(sizeof(t_pos))))
+		if (!(env = (t_env*)malloc(sizeof(t_env))))
 			return (-1);
-		init_array(array_pos, &av[1]);
-		init_env(env);
+		init_env(env, av[1]);
+		if (!(array_pos = (char***)malloc(sizeof(char) * env->len + 1)))
+			return (-1);
+		init_array(array_pos, &av[1], env->len);
 		while (env->y++ < 150)
 			mlx_pixel_put(env->mlx, env->win, env->x, env->y, 00255000000);
 		mlx_loop(env->mlx);	
