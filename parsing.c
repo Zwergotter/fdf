49e594fccfcf4447aaac68fcf6abcd7c612c1011
Esther DEVEZE <edeveze@student.42.fr>
2017-01-28 14:53:13 +0100

    parsing modified, is now working directly with input's height and ***mao

--- parsing.c
+++ parsing.c
@@ -12,48 +12,18 @@
 
 #include "fdf.h"
 
-t_pos   **setting_pos(t_pos **array_pos, char ***array)
-{   
-    int x;
-    int y;
-
-    x = 0;
-    y = 0;
-    while(array[y])
-    {
-        x = 0;
-        while(array[y][x])
-        {
-            (*array_pos)->x = x;
-            (*array_pos)->y = y;
-            (*array_pos)->z = ft_atoi(array[x][y]);
-            x++;
-            array_pos++;
-        }
-        y++;
-    }
-    return (array_pos);
-}
-
-t_pos    **read_file(int fd)
+char    ***read_file(char ***map, int fd, int array_size)
 {
     char *line = NULL;
-    char ***tmp;
-    int y;
-    t_pos  **array_pos;
+    int i;
     
-    y = 0;
-    if (!(tmp = (char***)malloc(sizeof(char))))
-        return (NULL);
+    i = 0;
+    map[array_size] = NULL;
     while (get_next_line(fd, &line) == 1)
     {
-        tmp[y++] = ft_strsplit(line, ' ');
+        map[i++] = ft_strsplit(line, ' ');
         free(line);
         line = NULL;
     }
-    if (!(array_pos = (t_pos**)malloc(sizeof(t_pos) * y )))
-        return (NULL);
-    array_pos = setting_pos(array_pos, tmp);
-    free(tmp);
-    return (array_pos);
+    return (map);
 }
