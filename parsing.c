44b0df66d18d6ce2d30bb980739122169a111fbc
Esther DEVEZE <edeveze@student.42.fr>
2017-02-09 11:50:09 +0100

    double check on parsed file added

--- parsing.c
+++ parsing.c
@@ -6,11 +6,31 @@
 /*   By: edeveze <edeveze@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/25 19:08:40 by edeveze           #+#    #+#             */
-/*   Updated: 2017/01/26 14:13:40 by edeveze          ###   ########.fr       */
+/*   Updated: 2017/02/09 11:46:23 by edeveze          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
+
+int     check_parsing(char **map)
+{
+    int x;
+    int y;
+
+    y = 0;
+    while (map[y])
+    {
+        x = 0;
+        while (map[y][x])
+        {
+            if (!(map[y][x] > 47 && map[y][x] < 58) || !(map[y][x] == '-'))
+                return (0);
+            x++;
+        }
+        y++;
+    }
+    return (1);
+}
 
 char    ***read_file(char ***map, int fd)
 {
@@ -19,10 +39,10 @@ char    ***read_file(char ***map, int fd)
     int i;
     int actual_len;
     int split_len;
-    
+
     i = -1;
     actual_len = 0;
-    while ((get_next_line(fd, &line)) > 0)
+    while ((get_next_line(fd, &line)) >= 0)
     {
         split_len = 0;
         temp = ft_strsplit(line, ' ');
@@ -34,7 +54,7 @@ char    ***read_file(char ***map, int fd)
         map[++i] = temp;
         free(line);
     }
-    if ((get_next_line(fd, &line)) < 0)
+    if ((get_next_line(fd, &line)) < 0 || !(check_parsing(*map)))
         return (NULL);
     return (map);
 }
