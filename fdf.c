f1f2f3d82c69cff4dfbab9bb85b2d9d093bd96df
Esther DEVEZE <edeveze@student.42.fr>
2017-02-09 11:59:57 +0100

    protection against files that don't exist added

--- fdf.c
+++ fdf.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   fdf.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cosi <cosi@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: edeveze <edeveze@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/24 17:29:35 by edeveze           #+#    #+#             */
-/*   Updated: 2017/02/08 15:12:23 by cosi             ###   ########.fr       */
+/*   Updated: 2017/02/09 11:58:40 by edeveze          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,13 +34,15 @@ void init_env(t_env *env, char *map)
 {
 	int fd;
 
-	fd = open(map, O_RDONLY);
+	if ((fd = open(map, O_RDONLY)) < 0)
+        exit (0);
 	env->win_x = 1200;
 	env->win_y = 800;
 	env->mlx = mlx_init();
 	env->win = mlx_new_window(env->mlx, env->win_x, env->win_y, "fdf");
 	env->len = map_length(fd);
-	env->zoom = 10;
+	env->zoom = 20;
+    env->move = 200;
 	env->depth = 0.1;
 	printf("in init_env - env address is %p\n", env);
 	printf("in init_env - env->mlx address is %p\n", env->mlx);
@@ -52,7 +54,8 @@ void init_array(char ***array_pos, char *file)
 		int		fd;
 
 		fd = open(file, O_RDONLY);
-		array_pos = read_file(array_pos, fd);
+		if (!(array_pos = read_file(array_pos, fd)))
+            exit (0);
 		close(fd);
 }
 
